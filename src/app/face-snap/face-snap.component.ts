92e15882dc160809766ae8f507b7006d427c95e4
ZORO <giral.zoro@mqashgroup.com>
2023-04-17 16:30:17 +0000

    First commit

---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,38 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FaceSnap } from '../models/face-snap.model';
+import { FaceSnapsService } from '../services/face-snaps.services';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-face-snap',
+  templateUrl: './face-snap.component.html',
+  styleUrls: ['./face-snap.component.scss'],
+})
+export class FaceSnapComponent implements OnInit {
+  @Input() faceSnap: FaceSnap;
+
+  buttonText: string;
+
+  constructor(
+    private faceSnapsService: FaceSnapsService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.buttonText = 'Oh Snap!';
+  }
+
+  onSnap() {
+    if (this.buttonText === 'Oh Snap!') {
+      this.faceSnapsService.snapFaceSnapById(this.faceSnap.id, 'snap');
+      this.buttonText = 'Oops, unSnap!';
+    } else {
+      this.faceSnapsService.snapFaceSnapById(this.faceSnap.id, 'unsanp');
+      this.buttonText = 'Oh Snap!';
+    }
+  }
+
+  onViewFaceSnaps() {
+    this.router.navigateByUrl(`facesnaps/${this.faceSnap.id}`);
+  }
+}
---
+++ src/app/services/face-snaps.services.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@angular/core';
+import { FaceSnap } from '../models/face-snap.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FaceSnapsService {
+  faceSnaps = [
+    {
+      id: 1,
+      title: 'Aurus',
+      description: 'Mon enfance reclame cette tendresse !',
+      createdDate: new Date(),
+      imageUrl:
+        'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+      snaps: 40,
+      location: 'Assinie',
+    },
+    {
+      id: 2,
+      title: 'Doggie un bon dog',
+      description: 'Mon compagnon fidèle !',
+      createdDate: new Date(),
+      imageUrl:
+        'https://cdn.pixabay.com/photo/2023/04/05/18/19/art-7902101_960_720.jpg',
+      snaps: 99,
+    },
+    {
+      id: 3,
+      title: 'En cas food',
+      description: 'La perfection se trouve dans les détails !',
+      createdDate: new Date(),
+      imageUrl:
+        'https://cdn.pixabay.com/photo/2023/03/22/20/16/muffin-7870491__340.jpg',
+      snaps: 101,
+      location: 'Abidjan',
+    },
+  ];
+
+  getAllFaceSnaps(): FaceSnap[] {
+    return this.faceSnaps;
+  }
+
+  getFaceSnapsById(faceSnapId: number): FaceSnap {
+    const faceSnap = this.faceSnaps.find(
+      (faceSnap) => faceSnap.id === faceSnapId
+    );
+    if (!faceSnap) {
+      throw new Error('FaceSnap not found');
+    } else {
+      return faceSnap;
+    }
+  }
+
+  snapFaceSnapById(faceSnapId: number, snapType: 'snap' | 'unsanp'): void {
+    const faceSnap = this.getFaceSnapsById(faceSnapId);
+    snapType === 'snap' ? faceSnap.snaps++ : faceSnap.snaps--;
+  }
+}
